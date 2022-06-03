983a65a525e9423b92c0c16e3d740ccc3c7e8f96
liu.changxi@datatom.com <pexgak-qedtif-vUmsy4>
2022-06-03 18:04:19 +0800

    使用m版本测试

--- ceph-client.rb
+++ ceph-client.rb
@@ -1,207 +1,118 @@
 class CephClient < Formula
   desc "Ceph client tools and libraries"
   homepage "https://ceph.com"
-  url "https://github.com/ceph/ceph.git", :using => :git, :revision => "0def4cf21d897bbf911614d8e3fe32e14fb30f7f"
-  version "quincy-17.0.0-4483-g0def4cf21d8"
+  url "https://github.com/ceph/ceph.git", :using => :git, :tag => "v13.2.2", :revision => "02899bfda814146b021136e9d8e80eba494e1126"
+  version "mimic-13.2.2"
 
-  bottle do
-    root_url "https://github.com/mulbc/homebrew-ceph-client/releases/download/quincy-17.0.0-4483"
-    sha256 cellar: :any, big_sur: "8796c05eaa6ac5065221ff6ba1b45201776666e5702f6573b8a3f857ba890d1f"
-  end
+  depends_on python if MacOS.version <= :snow_leopard
 
-  # depends_on "osxfuse"
-  depends_on "boost" => :build
   depends_on "openssl" => :build
   depends_on "cmake" => :build
-  depends_on "ninja" => :build
   depends_on "cython" => :build
   depends_on "leveldb" => :build
   depends_on "nss"
   depends_on "pkg-config" => :build
-  depends_on "python3"
-  depends_on "sphinx-doc" => :build
   depends_on "yasm"
-
-  resource "prettytable" do
-    url "https://files.pythonhosted.org/packages/d4/c6/d388b3d4992acf413d1b67101107b7f4651cc2835abd0bbd6661678eb2c1/prettytable-2.1.0.tar.gz"
-    sha256 "5882ed9092b391bb8f6e91f59bcdbd748924ff556bb7c634089d5519be87baa0"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
-    sha256 "b8eac752c5e14d3eca0e6dd9199cd627518cb5ec06add0de9d32baeee6fe645d"
-  end
 
   patch :DATA
 
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["nss"].opt_lib}/pkgconfig"
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_lib}/pkgconfig"
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", "#{Formula["cython"].opt_libexec}/lib/python#{xy}/site-packages"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
-    resources.each do |r|
-      r.stage do
-        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+    system "./do_cmake.sh",
+              "-DCMAKE_BUILD_TYPE=Debug",
+              "-DCMAKE_C_COMPILER=clang",
+              "-DCMAKE_CXX_COMPILER=clang++",
+              "-DDIAGNOSTICS_COLOR=always",
+              "-DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include",
+              "-DWITH_BABELTRACE=OFF",
+              "-DWITH_BLUESTORE=OFF",
+              "-DWITH_CCACHE=ON",
+              "-DWITH_CEPHFS=ON",
+              "-DWITH_EMBEDDED=OFF",
+              "-DWITH_KRBD=OFF",
+              "-DWITH_LIBCEPHFS=OFF",
+              "-DWITH_LTTNG=OFF",
+              "-DWITH_LZ4=OFF",
+              "-DWITH_MANPAGE=ON",
+              "-DWITH_RADOSGW=OFF",
+              "-DWITH_RDMA=OFF",
+              "-DWITH_SPDK=OFF",
+              "-DWITH_SYSTEMD=OFF",
+              "-DWITH_XFS=OFF"
+    system "make", "--directory=build", "rados", "rbd",  "manpages"
+    MachO.open("build/bin/rados").linked_dylibs.each do |dylib|
+      unless dylib.start_with?("/tmp/")
+        next
       end
+      MachO::Tools.change_install_name("build/bin/rados", dylib, "#{lib}/#{dylib.split('/')[-1]}")
     end
-
-    args = %W[
-      -DDIAGNOSTICS_COLOR=always
-      -DOPENSSL_ROOT_DIR=#{Formula["openssl"].opt_prefix}
-      -DWITH_BABELTRACE=OFF
-      -DWITH_BLUESTORE=OFF
-      -DWITH_CCACHE=OFF
-      -DWITH_CEPHFS=OFF
-      -DWITH_KRBD=OFF
-      -DWITH_LIBCEPHFS=ON
-      -DWITH_LTTNG=OFF
-      -DWITH_LZ4=OFF
-      -DWITH_MANPAGE=ON
-      -DWITH_MGR=OFF
-      -DWITH_MGR_DASHBOARD_FRONTEND=OFF
-      -DWITH_RADOSGW=OFF
-      -DWITH_RDMA=OFF
-      -DWITH_SPDK=OFF
-      -DWITH_SYSTEM_BOOST=ON
-      -DWITH_SYSTEMD=OFF
-      -DWITH_TESTS=OFF
-      -DWITH_XFS=OFF
-    ]
-    targets = %w[
-      rados
-      rbd
-      cephfs
-      ceph-conf
-      ceph-fuse
-      manpages
-      cython_rados
-      cython_rbd
-    ]
-    mkdir "build" do
-      system "cmake", "-G", "Ninja", "..", *args, *std_cmake_args
-      system "ninja", *targets
-      executables = %w[
-        bin/rados
-        bin/rbd
-        bin/ceph-fuse
-      ]
-      executables.each do |file|
-        MachO.open(file).linked_dylibs.each do |dylib|
-          unless dylib.start_with?("/tmp/")
-            next
-          end
-          MachO::Tools.change_install_name(file, dylib, "#{lib}/#{dylib.split('/')[-1]}")
-        end
+    MachO.open("build/bin/rbd").linked_dylibs.each do |dylib|
+      unless dylib.start_with?("/tmp/")
+        next
       end
-      %w[
-        ceph
-        ceph-conf
-        ceph-fuse
-        rados
-        rbd
-      ].each do |file|
-        bin.install "bin/#{file}"
-      end
-      %w[
-        ceph-common.2
-        ceph-common
-        rados.2.0.0
-        rados.2
-        rados
-        radosstriper.1.0.0
-        radosstriper.1
-        radosstriper
-        rbd.1.17.0
-        rbd.1
-        rbd
-        cephfs.2.0.0
-        cephfs.2
-        cephfs
-      ].each do |name|
-        lib.install "lib/lib#{name}.dylib"
-      end
-      %w[
-        ceph-conf
-        ceph-fuse
-        ceph
-        librados-config
-        rados
-        rbd
-      ].each do |name|
-        man8.install "doc/man/#{name}.8"
-      end
-      system "ninja", "src/pybind/install", "src/include/install"
+      MachO::Tools.change_install_name("build/bin/rbd", dylib, "#{lib}/#{dylib.split('/')[-1]}")
     end
-
+    bin.install "build/bin/ceph"
+    bin.install "build/bin/rados"
+    bin.install "build/bin/rbd"
+    lib.install "build/lib/libceph-common.0.dylib"
+    lib.install "build/lib/libceph-common.dylib"
+    lib.install "build/lib/librados.2.0.0.dylib"
+    lib.install "build/lib/librados.2.dylib"
+    lib.install "build/lib/librados.dylib"
+    lib.install "build/lib/libradosstriper.1.0.0.dylib"
+    lib.install "build/lib/libradosstriper.1.dylib"
+    lib.install "build/lib/libradosstriper.dylib"
+    lib.install "build/lib/librbd.1.12.0.dylib"
+    lib.install "build/lib/librbd.1.dylib"
+    lib.install "build/lib/librbd.dylib"
+    man8.install "build/doc/man/ceph-conf.8"
+    man8.install "build/doc/man/ceph.8"
+    man8.install "build/doc/man/librados-config.8"
+    man8.install "build/doc/man/rados.8"
+    man8.install "build/doc/man/rbd-ggate.8"
+    man8.install "build/doc/man/rbd-mirror.8"
+    man8.install "build/doc/man/rbd-nbd.8"
+    man8.install "build/doc/man/rbd-replay-many.8"
+    man8.install "build/doc/man/rbd-replay-prep.8"
+    man8.install "build/doc/man/rbd-replay.8"
+    man8.install "build/doc/man/rbd.8"
+    man8.install "build/doc/man/rbdmap.8"
+    ENV.prepend_create_path "PYTHONPATH", libexec
+    libexec.install "src/pybind/ceph_argparse.py"
+    libexec.install "src/pybind/ceph_daemon.py"
+    libexec.install "src/pybind/ceph_volume_client.py"
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-  end
-
-  def caveats; <<~EOS
-    The fuse version shipped with osxfuse is too old to access the
-    supplementary group IDs in cephfs.
-    Thus you need to add this to your ceph.conf to avoid errors:
-
-    [client]
-    fuse_set_user_groups = false
-
-    EOS
   end
 
   test do
     system "#{bin}/ceph", "--version"
-    system "#{bin}/ceph-fuse", "--version"
     system "#{bin}/rbd", "--version"
     system "#{bin}/rados", "--version"
-    system "python", "-c", "import rados"
-    system "python", "-c", "import rbd"
   end
 end
 
 __END__
-diff --git a/src/auth/KeyRing.cc b/src/auth/KeyRing.cc
-index 2ddc0b4ab22..2efb8b67a3b 100644
---- a/src/auth/KeyRing.cc
-+++ b/src/auth/KeyRing.cc
-@@ -205,12 +205,12 @@ void KeyRing::decode(bufferlist::const_iterator& bl) {
-   __u8 struct_v;
-   auto start_pos = bl;
-   try {
-+    decode_plaintext(start_pos);
-+  } catch (...) {
-+    keys.clear();
-     using ceph::decode;
-     decode(struct_v, bl);
-     decode(keys, bl);
--  } catch (ceph::buffer::error& err) {
--    keys.clear();
--    decode_plaintext(start_pos);
-   }
- }
+diff --git a/src/include/any.h b/src/include/any.h
+index 73e729b495..241e0e6d51 100644
+--- a/src/include/any.h
++++ b/src/include/any.h
+@@ -15,17 +15,10 @@
+#ifndef INCLUDE_STATIC_ANY
+#define INCLUDE_STATIC_ANY
 
-diff --git a/cmake/modules/Distutils.cmake b/cmake/modules/Distutils.cmake
-index 8dc69f0af51..0b2acaf160a 100644
---- a/cmake/modules/Distutils.cmake
-+++ b/cmake/modules/Distutils.cmake
-@@ -79,11 +79,9 @@ function(distutils_add_cython_module target name src)
-     OUTPUT ${output_dir}/${name}${ext_suffix}
-     COMMAND
-     env
--    CC="${PY_CC}"
-     CFLAGS="${PY_CFLAGS}"
-     CPPFLAGS="${PY_CPPFLAGS}"
-     CXX="${PY_CXX}"
--    LDSHARED="${PY_LDSHARED}"
-     OPT=\"-DNDEBUG -g -fwrapv -O2 -w\"
-     LDFLAGS=-L${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
-     CYTHON_BUILD_DIR=${CMAKE_CURRENT_BINARY_DIR}
-@@ -108,8 +106,6 @@ function(distutils_install_cython_module name)
-     set(CFLAG_DISABLE_VTA -fno-var-tracking-assignments)
-   endif()
-   install(CODE "
--    set(ENV{CC} \"${PY_CC}\")
--    set(ENV{LDSHARED} \"${PY_LDSHARED}\")
-     set(ENV{CPPFLAGS} \"-iquote${CMAKE_SOURCE_DIR}/src/include
-                         -D'void0=dead_function\(void\)' \
-                         -D'__Pyx_check_single_interpreter\(ARG\)=ARG \#\# 0' \
+-#if __has_include(<any>)
+-#include <any>
+-namespace ceph {
+-  using std::bad_any_cast;
+-}
+-#else
+#include <boost/any.hpp>
+namespace ceph {
+    using boost::bad_any_cast;
+}
+-#endif
 
+#include <cstddef>
+#include <initializer_list>
